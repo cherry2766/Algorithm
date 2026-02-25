11fcffe576ff6803679e49474bb356a07e8994c1
cherry <rlathrkd2766@gmail.com>
2026-02-25 17:30:54 +0900

    day01

---
+++ day01/src/SWEA/View/Solution.java
@@ -0,0 +1,20 @@
+package SWEA.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) throws FileNotFoundException {
+//		File file = new File("./src/SWEA/View/sample_input.txt");
+//		Scanner sc = new Scanner(file);
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt());
+		System.out.println(sc.nextInt());
+		System.out.println(sc.nextInt());
+		System.out.println(sc.nextInt());
+		System.out.println(sc.nextInt());
+
+	}
+}
---
+++ day01/src/SWEA/View/Solution2.java
@@ -0,0 +1,29 @@
+package SWEA.View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution2 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		// 바이트를 문자열로 변환
+		InputStreamReader isr = new InputStreamReader(System.in);
+		// 덩어리째 가져옴
+		BufferedReader br1 = new BufferedReader(isr);
+//		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+
+		// 한 줄 가져옴
+
+		String i1 = br1.readLine();
+
+		// 하나씩 탐색해서 살펴봄
+		StringTokenizer st1 = new StringTokenizer(br1.readLine());
+		StringTokenizer st2 = new StringTokenizer(i1);
+
+		String s = st1.nextToken();
+		int i = Integer.parseInt(st1.nextToken());
+
+	}
+}
