bcf4286204f54d39cdb41ceb017a923f1a342b0e
cherry <rlathrkd2766@gmail.com>
2026-03-10 10:32:11 +0900

    Solve SWEA 1966

---
+++ day08/src/SWEA/sortNumbers_1966/Solution.java
@@ -0,0 +1,36 @@
+package SWEA.sortNumbers_1966;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                list.add(Integer.valueOf(st.nextToken()));
+            }
+
+            Collections.sort(list);
+
+            System.out.print("#" + tc + " ");
+            for (int num : list) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
