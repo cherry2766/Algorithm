370016cff98164f9a33c83315330527fb68458aa
cherry <rlathrkd2766@gmail.com>
2026-03-11 12:28:36 +0900

    PGS: 문자열 출력하기

---
+++ Programmers/Lv0/PGS_181952/Solution.java
@@ -0,0 +1,10 @@
+package Lv0.PGS_181952;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str);
+    }
+}
