1e8013d205b8069b4bab52dda937a82e95a2eab9
cherry <rlathrkd2766@gmail.com>
2026-03-13 14:08:35 +0900

    PGS: 조건 문자열

---
+++ Programmers/Lv0/PGS_181934/Solution.java
@@ -0,0 +1,32 @@
+package Lv0.PGS_181934;
+
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        if (ineq.equals(">") && eq.equals("=")) {
+            if (n >= m) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (ineq.equals(">") && eq.equals("!")) {
+            if (n > m) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (ineq.equals("<") && eq.equals("=")) {
+            if (n <= m) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (ineq.equals("<") && eq.equals("!")) {
+            if (n < m) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        return 0;
+    }
+}
